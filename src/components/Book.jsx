db88a340d70e1b6e69e6891319deb470ce8bd20c
Blessing <blessingmike334@gmail.com>
2023-09-26 16:50:31 +0100

    Fix linters error

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/bookSlice';
 
-function Book({ title, author, category, item_id }) {
+function Book({
+  title, author, category, item_id,
+}) {
   const dispatch = useDispatch();
 
   const handleRemove = () => {

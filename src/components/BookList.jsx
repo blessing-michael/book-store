300f9c64a0b830274dc479d65fd64955ef3987e2
Blessing <blessingmike334@gmail.com>
2023-09-27 10:36:07 +0100

    Update readme

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,32 +3,7 @@ import Book from './Book';
 import AddBook from './AddBook';
 
 function Booklist() {
-  const books = [
-    {
-      id: 1,
-      title: 'Book 1',
-      category: 'Action',
-      author: ' Blessing Michael',
-      completed: '65%',
-      currentChapter: 'Chapter 3',
-    },
-    {
-      id: 2,
-      title: 'Book 2',
-      category: 'Fiction',
-      author: 'Martin Luther',
-      completed: '80%',
-      currentChapter: 'Chapter 1',
-    },
-    {
-      id: 3,
-      title: 'Book 3',
-      category: 'Economy',
-      author: 'Quin Din',
-      completed: '99%',
-      currentChapter: 'Chapter 10',
-    },
-  ];
+  const books = [];
 
   return (
     <div>

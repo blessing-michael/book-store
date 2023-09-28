a8318e6af44da402905179737740ff1427efabdb
Blessing <blessingmike334@gmail.com>
2023-09-28 10:13:36 +0100

    Fix linters error

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -21,8 +21,8 @@ const BookLists = () => {
       </div>
     );
   }
-  if (error) return <h2>Something went wrong</h2>;
-  if (books.length === 0) return <h2>No books available. Please come check later</h2>;
+  if (error) return <h2>Sorry! Something went wrong</h2>;
+  if (books.length === 0) return <h2>No books available now. Please come check later</h2>;
   return books.map((book) => (
     <div key={book.id}>
       <Book key={book.id} book={book} />

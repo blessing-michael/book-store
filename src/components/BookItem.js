3ce6c08e97bed0d810d87bd37f023b73d24e3f4e
Blessing <blessingmike334@gmail.com>
2023-09-29 10:42:36 +0100

    Add styles to bookstore

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -21,10 +21,10 @@ const BookLists = () => {
       </div>
     );
   }
-  if (error) return <h2>Sorry! Something went wrong</h2>;
-  if (books.length === 0) return <h2>No books available now. Please come check later</h2>;
+  if (error) return <h2>Something went wrong</h2>;
+  if (books.length === 0) return <h2>No books available. Please come check later</h2>;
   return books.map((book) => (
-    <div key={book.id}>
+    <div key={book.id} className="card">
       <Book key={book.id} book={book} />
     </div>
   ));
